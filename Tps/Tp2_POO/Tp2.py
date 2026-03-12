259660d31c1028d52a3e0982fd2882f012f8f07d
ouarda ait el fakih <ouardaaitelfakih@gmail.com>
2026-03-12 00:05:49 +0000

    ajout des partie 1,2,3,4,5 et 6 de TP2 de POO

---
+++ Tps/Tp2_POO/Tp2.py
@@ -0,0 +1,154 @@
+#|================================================================================================================|
+#|                                                                                                                |
+#|                                                  TP2                                                           |
+#|                                                                                                                |
+#|================================================================================================================|
+
+
+#=================================================================================================================
+#                                                  Partie1
+#=================================================================================================================
+
+#une classe abstraite est une classe qui définit une structure commune entre plusieurs classes ,mais elle ne peut pas être instanciée
+#les classes filles qui hérite doit fournir une implémentation concrète des méthodes abstraite dans la classe abstraite 
+
+#la classe de base 
+from abc import ABC, abstractmethod
+class Boisson(ABC):
+    @abstractmethod
+    def cout(self):
+        pass
+
+    @abstractmethod
+    def description(self):
+        pass
+
+    
+    #méthode addition
+    def __add__(self,other):
+        desc =self.description() + "+" +other.description()
+        prix = self.cout() + other.cout()
+    
+        return f"{desc},{prix}$"
+    
+    #méthode d'affichage
+    def afficher(self):
+        print(f"Commande:{self.description()}")
+        print(f"Prix : {self.cout()}")     
+
+#=================================================================================================================
+#                                                  Partie2
+#=================================================================================================================
+
+
+#créer des boissons concretes héritant de la classe abstraite Boisson
+
+#class Cafe
+class Cafe(Boisson):
+    def cout(self):
+        return 2.0
+    
+    def description(self):
+        return "Café simple"
+
+#class The
+class The(Boisson):
+    def cout(self):
+        return 1.5
+    
+    def description(self):
+        return "Thé"
+    
+# Il est obligatoire d'implémenter les méthodes dans les classe filles
+
+boisson = Cafe()            #la classe fille peut être instanciée
+
+print(boisson.description())
+print(boisson.cout())
+
+
+
+#=================================================================================================================
+#                                                  Partie3
+#=================================================================================================================
+
+
+#une classe décorateur a pour objective de modifier le comportement des classes existante
+class DecorateurBoisson(Boisson):
+
+    def __init__(self,boisson):
+        self._boisson = boisson
+
+#ajout de lait
+class Lait(DecorateurBoisson):
+    def cout(self):
+        return self._boisson.cout()+0.5
+    
+    def description(self):
+        return self._boisson.description()+",Lait"
+
+#ajout de sucre
+class Sucre(DecorateurBoisson):
+
+    def cout(self):
+        return self._boisson.cout() + 0.2
+    
+    def description(self):
+        return self._boisson.description() + ",Sucre"
+
+#exemple:
+boisson1 = The()
+boisson1 = Lait(boisson1)
+boisson1 = Sucre(boisson1)
+
+print(boisson1.description())
+print(boisson1.cout())
+
+#=================================================================================================================
+#                                                  Partie4
+#=================================================================================================================
+
+#on fait la surcharge de l'opération + pour deux boissons
+#voir la ligne 28
+#exemple:
+menu = boisson + boisson1
+print(menu)
+
+
+#=================================================================================================================
+#                                                  Partie5
+#=================================================================================================================
+
+#data classes sont des classes qui contenat uniquement des données
+from dataclasses import dataclass
+
+@dataclass
+class Client:
+    nom: str
+    numero: int 
+    points_fidelite: int
+
+
+#=================================================================================================================
+#                                                  Partie6
+#=================================================================================================================
+
+
+#1.Ajouter un ingrédient supplémentaire
+#ajout de Caramel
+class Caramel(DecorateurBoisson):
+
+    def cout(self):
+        return self._boisson.cout() + 0.5
+    
+    def description(self):
+        return self._boisson.description() + ",Caramel"
+    
+#2.Implémenter la combinaison de boissons avec +
+#déjà fait dans la partie 4
+
+#3.ajouter une méthode permettant d'afficher une commande complète
+#voir la ligne 34
+#exemple:
+print(boisson1.afficher())
+
